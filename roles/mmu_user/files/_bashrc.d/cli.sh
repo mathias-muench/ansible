52aa51b7c6181b5855da816b89dff800d267bfc0
Mathias Muench <mathias.muench@mathias-muench.de>
2021-10-23 11:06:39 +0200

    Try to automatically recover bash history

--- roles/mmu_user/files/_bashrc.d/cli.sh
+++ roles/mmu_user/files/_bashrc.d/cli.sh
@@ -6,6 +6,7 @@ case "$-" in
 		PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 		HISTSIZE=100000
 		HISTFILESIZE=1000000
+		test -r ~/.local/share/bash_history && test $(wc -l <~/.local/share/bash_history) -gt $(history | wc -l) && history -r ~/.local/share/bash_history
 		;;
 esac
 

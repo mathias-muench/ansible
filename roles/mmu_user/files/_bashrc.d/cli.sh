69b7da83c6d91de874156297c0df297c6d2216cb
Mathias Muench <mathias.muench@mathias-muench.de>
2025-10-15 07:42:22 +0200

    gh alias

--- roles/mmu_user/files/_bashrc.d/cli.sh
+++ roles/mmu_user/files/_bashrc.d/cli.sh
@@ -5,7 +5,7 @@ case "$-" in
 		alias dv="dirs -v"
 
 		trap 'history -w ~/.local/share/bash_history' EXIT
-		PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+		PROMPT_COMMAND='echo -ne "\033]0;$PWD\007";history -a;'$PROMPT_COMMAND
 		HISTSIZE=100000
 		HISTFILESIZE=1000000
 		test -r ~/.local/share/bash_history && test $(wc -l <~/.local/share/bash_history) -gt $(history | wc -l) && history -r ~/.local/share/bash_history

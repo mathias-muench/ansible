59a30e337bd8edf2434bb69ca56e10700209f4b3
Mathias Münch <mathias-muench@users.noreply.github.com>
2026-09-13 14:22:17 +0200

    .

--- mmu.ps1
+++ mmu.ps1
@@ -6,3 +6,7 @@ $L = if ($DryRun) { "/L" } else { $null }
 
 & robocopy "$PSScriptRoot\files\_config\opencode\skills" "$env:USERPROFILE\.config\opencode\skills" /NJH /NJS $L
 & robocopy "$PSScriptRoot\files\_config\opencode" "$env:USERPROFILE\.config\opencode" opencode.jsonc /NJH /NJS $L
+
+New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\nvim\autoload"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile "$env:LOCALAPPDATA\nvim\autoload\plug.vim"
+& robocopy "$PSScriptRoot\files\_config\nvim" "$env:LOCALAPPDATA\nvim" init.vim /NJH /NJS $L
--- mmu.sh
+++ mmu.sh
@@ -31,7 +31,8 @@ fi
 rsync -avc $1 "$FILES_DIR/_bashrc.d/" ~/.bashrc.d/
 rsync -avc $1 "$FILES_DIR/_config/git/" ~/.config/git/
 rsync -avc $1 "$FILES_DIR/_config/gh/" ~/.config/gh/
-rsync -avc $1 --delete "$FILES_DIR/_config/opencode/" ~/.config/opencode/
+rsync -avc $1 --delete "$FILES_DIR/_config/opencode/skills/" ~/.config/opencode/skills/
+rsync -avc $1 "$FILES_DIR/_config/opencode/opencode.jsonc" ~/.config/opencode/opencode.jsonc
 rsync -avc $1 "$FILES_DIR/_config/pandoc/" ~/.local/share/pandoc/
 
 mkdir -p ~/.config/nvim/autoload
